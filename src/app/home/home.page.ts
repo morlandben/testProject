ee5d3aa16af92b3a32fb46a1913ca75236be3b27
Ben Morland <benmorland@Bens-MacBook-Air.local>
2022-06-22 15:06:24 -0500

    maydevelop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+  inputValue1: number = -1
+  inputValue2: number = -1
+  result: number
+  saveOperator: string = ""
+  outputString: string = ""
+  //these buttons do the multiplying and shit
+  math(value: number) {
+    this.printOut(value + "")
+    if (this.inputValue1 == -1) { 
+      this.inputValue1 = value 
+    }
+    else {
+      this.inputValue2 = value 
+    }
+    
+    console.log(this.inputValue1)
+      console.log(this.inputValue2)
 
+  }
+  operators(op: string) {
+   this.printOut(op)
+    if (op == '=') {
+      console.log(this.inputValue1)
+      console.log(this.inputValue2)
+
+      if (this.saveOperator == "+") {
+        this.result = this.inputValue1 + this.inputValue2
+      }
+      if (this.saveOperator == "-") {
+        this.result = this.inputValue1 - this.inputValue2
+      }
+      if (this.saveOperator == "/") {
+        this.result = this.inputValue1 / this.inputValue2
+      }
+      if (this.saveOperator == "*") {
+        this.result = this.inputValue1 * this.inputValue2
+      }
+      if (this.saveOperator == "sqrt") {
+        this.result = Math.sqrt(this.inputValue1)
+      }
+      this.inputValue1 = -1
+      this.inputValue2 = -1
+    }
+    this.saveOperator = op
+  }
+  printOut(op: string){
+this.outputString += op
+  }
 }
